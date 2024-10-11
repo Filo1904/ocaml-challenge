306e536f538a2d75baf01f26ea1f08d139d22fb2
Filo1904 <filo1904.business@gmail.com>
2024-10-11 12:13:17 +0200

    Primo esercizio

---
+++ 1/nand/nand.ml
@@ -0,0 +1,20 @@
+
+(*Soluzione 1*)
+let nand1 (a : bool) (b : bool) : bool = not(a && b)
+
+(*Soluzione 2*)
+let nand2 (a : bool) (b : bool) : bool =
+  if a 
+    then(
+      if b
+        then false
+        else true
+      )
+else ture
+
+(*Soluzione 3*)
+
+let nand3 (a : bool) (b : bool) : bool =
+  match(a,b) whit
+  | (true, true) -> false 
+  | _ -> true
